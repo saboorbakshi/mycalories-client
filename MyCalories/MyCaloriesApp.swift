71e08cc1ace29f1933684addd209f7d3d091fcea
saboorbakshi <saboor.bakshi@uwaterloo.ca>
2025-10-26 18:36:32 -0400

    Initial Commit

---
+++ MyCalories/MyCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCaloriesApp.swift
+//  MyCalories
+//
+//  Created by Saboor Bakshi on 2025-10-26.
+//
+
+import SwiftUI
+
+@main
+struct MyCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
